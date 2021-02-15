d14c5d860fdd119a60a41e6ad658f4d96f77d75d
Mark Railton <markjasonrailton@gmail.com>
2021-02-15 12:30:21 +0000

    updates

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "azurerm_virtual_network_id" {
 }
 
 output "azurerm_virtual_network_name" {
-  value = azurerm_virtual_network.vnet.vnet.name
+  value = azurerm_virtual_network.vnet.name
 }
 
 output "azurerm_subnet_id_1" {
