4433c34f351c0149a91a9f59e2eecacd22d865ab
Mark Railton <markjasonrailton@gmail.com>
2021-02-18 14:49:31 +0000

    updates

--- main.tf
+++ main.tf
@@ -3,19 +3,6 @@ resource "random_string" "enigma" {
   special = false
 }
 
-resource "azurerm_network_ddos_protection_plan" "ddos" {
-  name                = "${local.name}-ddos-${lower(random_string.enigma.result)}"
-  location            = var.location
-  resource_group_name = var.resourcegroup
-  
-  tags = {
-    Environment = var.environment
-    BuildBy     = var.tag_buildby
-    BuildTicket = var.tag_buildticket
-    BuildDate   = var.tag_builddate
-  }
-}
-
 resource "azurerm_virtual_network" "vnet" {
   name                = "${local.name}-vnet-${lower(random_string.enigma.result)}"
   location            = var.location
@@ -34,25 +21,4 @@ resource "azurerm_virtual_network" "vnet" {
     BuildTicket = var.tag_buildticket
     BuildDate   = var.tag_builddate
   }
-}
-
-resource "azurerm_subnet" "sub1" {
-  name                 = "${local.name}-sub1-${lower(random_string.enigma.result)}"
-  resource_group_name  = var.resourcegroup
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_subnet" "sub2" {
-  name                 = "${local.name}-sub2-${lower(random_string.enigma.result)}"
-  resource_group_name  = var.resourcegroup
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_subnet" "sub3" {
-  name                 = "${local.name}-sub3-${lower(random_string.enigma.result)}"
-  resource_group_name  = var.resourcegroup
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.3.0/24"]
 }
