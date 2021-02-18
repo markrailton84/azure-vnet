8d178f2faba976fd1b38cd216b45b0acc2e25b41
Mark Railton <markjasonrailton@gmail.com>
2021-02-18 15:22:30 +0000

    multiple updates

---
+++ tests/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-azure-tf"
+    storage_account_name = "saazuretf"
+    container_name       = "terraform-state"
+    key                  = "terraform-vnet.tfstate"
+  }
+}
+
+resource "random_string" "enigma" {
+  length  = 6
+  special = false
+}
+
+resource "azurerm_resource_group" "resource_group" {
+ name = "${local.name}-rsg-${lower(random_string.enigma.result)}"
+ location = var.location
+}
+
+# Root Module Calling
+
+module "vnet_testing" {
+source = "../"
+name = "${local.name}-vnet-${lower(random_string.enigma.result)}"
+location = var.location
+resource_group_name = var.resource_group
+depends_on = [azurerm_resource_group.resource_group]
+}
---
+++ tests/variables.tf
@@ -0,0 +1,45 @@
+# Base variables
+
+variable "location" {
+  type        = string
+  description = "Location of the resources"
+  default     = "UK South"
+}
+
+variable "resource_group" {
+    type = string
+    description = "static rsg"
+    default = "markrsg"
+} 
+
+# Tag variables
+
+variable "tag_buildby" {
+  type        = string
+  description = "Name of the builder."
+  default     = "Mark Railton"
+}
+
+variable "tag_buildticket" {
+  type        = string
+  description = "Ticket Number for the build"
+  default     = "no ticket"
+}
+
+variable "tag_builddate" {
+  type        = string
+  description = "Date in ISO-8601 format (yyyymmdd)."
+  default     = "20210119"
+}
+
+variable "environment" {
+  type        = string
+  description = "Prod,QA,STG,DEV,etc."
+  default     = "DEV"
+}
+
+variable "publisher_email" {
+  type        = string
+  description = "Publisher email"
+  default     = "mark.railton@rackspace.com"
+}
